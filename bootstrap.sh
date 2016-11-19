f2ca7fac4911cc48cc131c733f8c92c2370a95da
Reuben Avery <ravery@bitswarm.io>
2016-11-19 14:57:33 -0500

    bugfixing

--- bootstrap.sh
+++ bootstrap.sh
@@ -14,10 +14,10 @@ FACTER_provisioner=$provisioner \
 FACTER_puppetversion=puppetversion \
 FACTER_app_project='puppetmaster' \
 FACTER_app_creator=$app_creator \
-puppet apply ${DIR}/manifests/r10k_bootstrap.pp \
+puppet apply "${DIR}/manifests/r10k_bootstrap.pp" \
     --show_diff \
     --verbose \
-    --hiera_config_path=${DIR}/bootstrap/hiera.yaml
+    --hiera_config="${DIR}/bootstrap/hiera.yaml" \
     --modulepath="${DIR}/modules:/etc/puppetlabs/code/modules:/opt/puppetlabs/puppet/modules"
 
 set +x
@@ -29,10 +29,10 @@ FACTER_provisioner=$provisioner \
 FACTER_puppetversion=$puppetversion \
 FACTER_app_project='puppetmaster' \
 FACTER_app_creator=$app_creator \
-puppet apply $${DIR}/environments/aws/manifests/base.pp \
+puppet apply "${DIR}/environments/aws/manifests/base.pp" \
     --show_diff \
     --verbose \
-    --hiera_config_path=$${DIR}/bootstrap/hiera.yaml \
+    --hiera_config="${DIR}/bootstrap/hiera.yaml" \
     --modulepath="${DIR}/modules:/etc/puppetlabs/code/modules:/opt/puppetlabs/puppet/modules"
 
 set +x
